4eb40ff4982a20e0de9a18c3ea0dba09d2d92255
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-04-20 20:05:47 -0230

    2022-04-20-5
    Finished adding in the wood facts.

--- script.js
+++ script.js
@@ -148,13 +148,13 @@ function caculation() {
 function chooseWoodFact() {
     const selectedWoodSpecies =  woodSpecies.options[woodSpecies.selectedIndex].text;
     if (selectedWoodSpecies == "Birch - White") {
-        return aFact = 'White Birch .'  
+        return aFact = "White Birch has pale sapwood and it's heartwood light to dark redish-brown and can sometimes be heavily figured. Due the fact it's density is lower than that of yellow birch, white birch is not as commonly used in cabinetwork."   
     }
     else if (selectedWoodSpecies == "Birch - Yellow") {
-        return aFact = 'Yellow Birch is pale and the sapwood is light to dark redish-brown and can sometimes be heavily figured. It is a moderlty hard and heavy wood, which is used in applications such as flooring, toys, doors, trim, cutting boards and toothpicks. Its relatively low price and good woodworking qualities make it ideal for cabinetwork.'  
+        return aFact = "Yellow Birch has pale sapwood and it's heartwood light to dark redish-brown and can sometimes be heavily figured. It is a moderlty hard and heavy wood, which is used in applications such as flooring, toys, doors, trim, cutting boards and toothpicks. Its relatively low price and good woodworking qualities make it ideal for cabinetwork."  
     }
     else if (selectedWoodSpecies == "Ceader - Western Red") {
-        return aFact = 'Ceader .'  
+        return aFact = 'Ceader is a low density soft wood. Due to its rot resitant nature it is often used in outdoor applications. Ceader wood also produces a lovely scent.'  
     }
     else if (selectedWoodSpecies == "Cherry") {
         return aFact = "Cherry wood was once known as poor man's mahgoany. Freshly cut the heartwood is pinkish-brown but ages to a lovely reddish-brown. It has a density that is hard enough to wear well but yet is still easy to work with. It is used for all kinds of furniture, cabinet as well as turnings and veneer."  
@@ -163,37 +163,37 @@ function chooseWoodFact() {
         return aFact = 'Douglas Fir is a strong wood which is relatively easy to work. It is used commonly in the construction trades for things such as studs, beams, posts, flooring and siding in houses. Douglas Fir is also fairly decay resistant.'  
     }
     else if (selectedWoodSpecies == "Elm - American") {
-        return aFact = 'American Elms grain is interlocked as it grows clockwise for a few years then counter clockwise, this makes it difficult to chisel and plane. Commercially it is used for products such as boxes and kitchen cabinets. Its strength and toughness make it useful for furniture.'  
+        return aFact = "American Elms' grain is interlocked as it grows clockwise for a few years then counter clockwise, this makes it difficult to chisel and plane. Commercially it is used for products such as boxes and kitchen cabinets. Its strength and toughness make it useful for furniture."  
     }
     else if (selectedWoodSpecies == "Hickory") {
-        return aFact = 'Hickory .'  
+        return aFact = 'Hickories various types are almost impossible to distingiush. It is a very heavy and hard wood a extremely tough and elastic ring-porous wood. It is most commonly used in tool handles and is sometimes used for cabintes. Hickory is also used as fuel for smoking meat as it adds a lovely flavour.'  
     }
     else if (selectedWoodSpecies == "Maple - Hard") {
-        return aFact = 'Hard Maple .'  
+        return aFact = "Hard Maple is also known as sugar maple, it is has creamy white sapwood and light gray or reddish-brown heartwood. It often is found to have wavy, curly and bird's eye figure. The wood is diffuseporous, hard and heavy. It is used in applications where hard, long-wearing material is desired. It is also commonly used as a material for musical instruments."  
     }
     else if (selectedWoodSpecies == "Maple - Soft") {
-        return aFact = 'Soft Maple .'  
+        return aFact = "Soft Maple is not as heavy and dense as hard maple. It's sapwood is light in colour and the heartwood light brown. Soft maple can sometimes have quilted or curly grain. It glues, finishes and turns very well and is used in all types of furniture as well as interior trim, millwork and veneer. It is a versatile wood that is economical and readily available."  
     }
     else if (selectedWoodSpecies == "Oak - Red") {
         return aFact = 'Red Oak is not as decay resistant as white oak so is used mainly in interior products, such as furniture and cabinets. The wood planes and polishes well but will tear on the lathe. Milling of sharp edges in oak should be avoid as they tend to break under wear and use.'  
     }
     else if (selectedWoodSpecies == "Oak - White") {
-        return aFact = 'White Oak is a very decay-resistant wood and is quite heavy and hard. It is a good wood for outdoor use but is also used for a large variety of indoor prodcuts, such as millwork, furniture, flooring. Oak has a reputation of being a difficult wood to dry and is often left to dry out after use.'  
+        return aFact = 'White Oak is a very decay-resistant wood and is quite heavy and hard. It is a good wood for outdoor use but is also used for a large variety of indoor prodcuts, such as millwork, furniture, flooring. Oak has a reputation of being a difficult wood to dry.'  
     }
     else if (selectedWoodSpecies == "Poplar") {
-        return aFact = 'Poplar has a very pale almost white sapwood white its heartwood is a bit dark and the grain is fairly straight. It is used in items such as boxes, venner, plywood and pallets. Poplar is unfoutantly not a decay resistant wood.'  
+        return aFact = 'Poplar has a very pale almost white sapwood white its heartwood is a bit dark and the grain is fairly straight. It is used in items such as boxes, veneer, plywood and pallets. Poplar is unfortunately not a decay resistant wood.'  
     }
     else if (selectedWoodSpecies == "Spruce - Black") {
-        return aFact = 'Spruce .'  
+        return aFact = 'Black Spruce has both sapwood and heartwood that is light in colour. It is one of the primer sources of pulpwood for paper. It is also one of the mostly commonly used timbers for constrution.'  
     }
     else if (selectedWoodSpecies == "Walnut - Black") {
         return aFact = 'Walnut is very decay resistant wood and was used for railway ties for a period of time. It is a very easy wood to work with and produces lovely cabinets and veneer.'  
     }
     else if (selectedWoodSpecies == "White Pine") {
-        return aFact = 'White Pine, when quater sawn is the most well behaved of native Canadian woods. The ease of sanding, chiseling and plaing of pine makes it an ideal secondary or even primary wood for many projects. As an added bonus it produces and lovely clean woody odor.'  
+        return aFact = 'White Pine, when quater sawn is the most well behaved of native Canadian woods. The ease of sanding, chiseling and planing of pine makes it an ideal secondary or even primary wood for many projects. As an added bonus it produces and lovely clean woody odor.'  
     }
     else {
-        return aFact = 'a tree is wood.'
+        return aFact = 'A dominate use of wood is for furniture making and constrution.'
     }
 }
 
